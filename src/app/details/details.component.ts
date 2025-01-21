9c693be46cf10ca6dc1a8cbeb5d03e566d7a0eed
silver-it <alex.jin220@gmail.com>
2025-01-21 08:36:16 +0800

    feat: added routes

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
+
+@Component({
+  selector: 'app-details',
+  imports: [CommonModule],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocation: HousingLocation | undefined;
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    const housingLocationId = Number(this.route.snapshot.params['id']);
+    this.housingLocation = this.housingService.getHousingLocationById(housingLocationId);
+
+  }
+}
